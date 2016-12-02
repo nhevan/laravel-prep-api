c7cef89413b6079d8d6f0874cc09ccd4e2b9a790
NH Evan <nhevan@gmail.com>
2016-12-03 00:07:05 +0600

    Update factories to include \Tag and \LessonTag

--- tests/factories/factories.php
+++ tests/factories/factories.php
@@ -1,5 +1,22 @@
 <?php 
+
+use App\Tag;
+use App\Lesson;
+
 $factory('App\Lesson', [
     'title' => $faker->sentence(3),
     'body' => $faker->paragraph(3)
 ]);
+
+$factory('App\Tag', [
+    'name' => $faker->word()
+]);
+
+$factory('App\LessonTag', function($faker){
+	$lessonIds = Lesson::pluck('id');
+	$tagIds = Tag::pluck('id');
+	return [
+	    'lesson_id' => $faker->randomElement($lessonIds->toArray()),
+	    'tag_id' => $faker->randomElement($tagIds->toArray())
+	];
+});

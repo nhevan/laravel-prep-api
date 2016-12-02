ee58bf1fbb7465a84eefee45440ff3b9c98ed237
NH Evan <nhevan@gmail.com>
2016-12-02 18:05:11 +0600

    Update Lesson Controller with index(), show() and store() api calls

--- app/Http/Controllers/LessonsController.php
+++ app/Http/Controllers/LessonsController.php
@@ -6,28 +6,65 @@ use App\Lesson;
 use Illuminate\Http\Request;
 use Acme\Transformers\LessonTransformer;
 
-class LessonsController extends Controller
+class LessonsController extends ApiController
 {
     protected $lesson;
+    
+    /**
+     * Acme/Transformers/lessonTransformer
+     * @var lessonTransformer
+     */
     protected $lessonTransformer;
+
     public function __construct(Lesson $lesson, LessonTransformer $lessonTransformer)
     {
         $this->lesson = $lesson;
         $this->lessonTransformer = $lessonTransformer;
     }
+
+    /**
+     * lists all lessons
+     * @return response lessons array
+     */
     public function index()
     {
         $lessons = $this->lesson->all();
 
-        return response([
+        return $this->respond([
             'data'=>$this->lessonTransformer->transformCollection($lessons->toArray())
         ]);
     }
 
-    public function show(Lesson $lesson)
+    /**
+     * shows a specific lesson
+     * @param  int $id lesson's id
+     * @return response
+     */
+    public function show($id)
     {
-        return response([
+        $lesson = $this->lesson->find($id);
+        if (!$lesson) {
+            return $this->responseNotFound('Lesson does not exist.');
+        }
+        return $this->respond([
             'data'=> $this->lessonTransformer->transform($lesson)
         ]);
+    }
+    /**
+     * stores a new lesson
+     * @param  Request $request 
+     * @return Response         
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ];
+        if (!$this->setRequest($request)->isValidated($rules)) {
+        	return $this->responseValidationError();
+        }
+        $this->lesson->create($request->all());
+        return $this->respondCreated('Lesson Created Successfully.');
     }
 }

fc0c0dd0584997a76f17acfce93ceb26cc5651aa
NH Evan <nhevan@gmail.com>
2016-12-03 00:33:06 +0600

    Update and refactor DatabaseSeeder class

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Tag;
+use App\Lesson;
+use App\LessonTag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+	/**
+	 * @var array
+	 */
+	private $tables = [
+		'lessons',
+		'tags',
+		'lesson_tag'
+	];
     /**
      * Run the database seeds.
      *
@@ -11,6 +22,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+    	$this->clearDb();
+    	
         $this->call('LessonsTableSeeder');
+        $this->call('TagsTableSeeder');
+        $this->call('LessonTagTableSeeder');
+    }
+
+    /**
+     * Clear entire database
+     * @return void 
+     */
+    public function clearDb()
+    {
+    	DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    	foreach ($this->tables as $tableName) {
+    		DB::table($tableName)->truncate();
+    	}
+		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
